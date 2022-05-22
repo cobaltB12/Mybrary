ae4b2512bd294e00bf864919345dc641d8104966
cobaltB12 <puppyfat2@gmail.com>
2022-05-22 15:11:35 +0800

    File Upload Database

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,10 @@
 const express = require('express')
 const router = express.Router()
-//const path = require('path')
-//const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const { query } = require('express')
-
-// const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-// const upload = multer({
-//     dest: uploadPath,
-//     fileFilter:(req, file, callback)=>{
-//         callback(null, imageMimeTypes.includes(file.mimetype))
-//     }
-// })
+
 
 // All books route
 router.get('/', async(req, res) => {
@@ -46,14 +37,12 @@ router.get('/new', async(req, res) =>{
 
 //create book route
 router.post('/', async(req, res)=>{
-    // const fileName = req.file != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title, 
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
         description: req.body.description
-        //coverImageName: fileName
     })
     saveCover(book, req.body.cover)
 
@@ -62,9 +51,6 @@ router.post('/', async(req, res)=>{
         //res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     }catch{
-        // if(book.coverImageName != null ){
-        //     removeBookCover(book.coverImageName)
-        // }  
         renderNewPage(res, book, true)
     }
 })
@@ -82,12 +68,6 @@ async function renderNewPage(res, book, hasError = false){
         res.redirect('/books') 
     }
 }
-
-// function removeBookCover(fileName){
-//     fs.unlink(path.join(uploadPath, fileName), err =>{
-//         if (err) console.error(err)
-//     })
-// }
 
 function saveCover(book, coverEncoded){
     if(coverEncoded == null) return 
